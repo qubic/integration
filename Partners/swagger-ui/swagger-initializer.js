b4178b9254508f4f40b5b00aa9455f2c7dd1bd3a
mio <180114449+qubicmio@users.noreply.github.com>
2025-05-27 08:41:32 +0000

    Revert "added qubic transfers swagger docs" (#41)
    * This reverts commit 92b48fb0
    * Qubic transfers api documentation should not be live (see issue #40).

--- Partners/swagger-ui/swagger-initializer.js
+++ Partners/swagger-ui/swagger-initializer.js
@@ -7,7 +7,6 @@ window.onload = function() {
       {url: "archive.swagger.json", name: "Qubic RPC Archive Tree"},
       {url: "qubic-http.swagger.json", name: "Qubic RPC Live Tree"},
       {url: "stats-api.swagger.json", name: "Qubic Stats API"},
-      {url: "qubic-transfers.swagger.json", name: "Qubic Transfers API"},
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
